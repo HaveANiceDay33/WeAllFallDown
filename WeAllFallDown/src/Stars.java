5b148eb46cf6c9cf6c8ff46c3a728727258c07a2
HaveANiceDay33 <munrosamuel@gmail.com>
2017-05-10 21:34:45 -0400

    the star paralax class

---
+++ WeAllFallDown/src/Stars.java
@@ -0,0 +1,21 @@
+import org.newdawn.slick.Color;
+
+import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
+
+public class Stars {
+	
+	public float x;
+	public float y;
+	public float speed;
+	
+	public Stars(float x, float y, float speed){
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+	}
+	public void display(float delta){
+		this.y += this.speed * delta * 10;
+		HvlPainter2D.hvlDrawQuad(x, y, speed, speed, new Color(255, 255, 255, .7f));
+	}
+	
+}
