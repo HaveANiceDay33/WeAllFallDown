b73496b1fe4d98437a66f08730e2535dea17b8da
Sam M <munrosamuel@gmail.com>
2017-09-06 08:47:09 -0400

    Added shot class for work at home

---
+++ WeAllFallDown/src/com/samuel/Shot.java
@@ -0,0 +1,22 @@
+package com.samuel;
+
+import org.newdawn.slick.Color;
+
+import com.osreboot.ridhvl.painter.painter2d.HvlPainter2D;
+
+public class Shot {
+	
+	public float x;
+	public float y;
+	public float speed;
+	
+	public Shot(float x, float y, float speed) {
+		this.x = x;
+		this.y = y;
+		this.speed = speed;
+	}
+	public void display(float delta) {
+		this.y -= this.speed * delta;
+		HvlPainter2D.hvlDrawQuad(x, this.y, 5,25, new Color(100 + (int)(Math.random() * ((255 - 100) + 1)) ,100 + (int)(Math.random() * ((255 - 100) + 1)),100 + (int)(Math.random() * ((255 - 100) + 1))));
+	}
+}
